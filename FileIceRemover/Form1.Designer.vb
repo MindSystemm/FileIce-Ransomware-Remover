2c409df24eef0554ff48ac4a99dd9f34bd084517
MindSystemm <mindlockreverser@gmail.com>
2017-07-21 02:13:05 +0200

    Initial repo
    .

---
+++ FileIceRemover/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(110, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(224, 55)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Remove the Ransomware ! "
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(107, 89)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(230, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "[*] MindSystem FileIce Ransomware remover [*]"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(451, 115)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "MindSystem FileIce Ransomware Remover  "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ FileIceRemover/Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+    Declare Function UnhookWindowsHookEx Lib "user32" Alias "UnhookWindowsHookEx" (ByVal hHook As IntPtr) As Boolean
+    Delegate Function LowLevelKeyboardProcDelegate(ByVal nCode As Integer, ByVal wParam As Integer, ByRef lParam As KBDLLHOOKSTRUCT) As Integer
+    Declare Function SetWindowsHookEx Lib "user32" Alias "SetWindowsHookExA" (ByVal idHook As Integer, ByVal lpfn As LowLevelKeyboardProcDelegate, ByVal hMod As IntPtr, ByVal dwThreadId As Integer) As IntPtr
+    Declare Function CallNextHookEx Lib "user32" Alias "CallNextHookEx" (ByVal hHook As IntPtr, ByVal nCode As Integer, ByVal wParam As Integer, ByRef lParam As KBDLLHOOKSTRUCT) As Integer
+    Const WH_KEYBOARD_LL As Integer = 13
+    Dim intLLKey As IntPtr
+    Structure KBDLLHOOKSTRUCT
+        Dim vkCode As Integer
+        Dim scanCode As Integer
+        Dim flags As Integer
+        Dim time As Integer
+        Dim dwExtraInfo As Integer
+    End Structure
+    Private Function LowLevelKeyboardProc(ByVal nCode As Integer, ByVal wParam As Integer, ByRef lParam As KBDLLHOOKSTRUCT) As Integer
+        Dim blnEat As Boolean = False
+
+        Select Case wParam
+            Case 256, 257, 260, 261
+                blnEat = ((lParam.vkCode = 9) AndAlso (lParam.flags = 32)) Or
+                ((lParam.vkCode = 27) AndAlso (lParam.flags = 32)) Or
+                ((lParam.vkCode = 27) AndAlso (lParam.flags = 0)) Or
+                ((lParam.vkCode = 91) AndAlso (lParam.flags = 1)) Or
+                ((lParam.vkCode = 92) AndAlso (lParam.flags = 1))
+        End Select
+
+        If blnEat = True Then
+            Return 1
+        Else
+            Return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam)
+        End If
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        intLLKey = SetWindowsHookEx(WH_KEYBOARD_LL, AddressOf LowLevelKeyboardProc, IntPtr.Zero, 0)
+        UnhookWindowsHookEx(intLLKey)
+        Interaction.Shell("explorer.exe")
+        My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System", "DisableLockWorkstation", "0", Microsoft.Win32.RegistryValueKind.DWord)
+        My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System", "DisableChangePassword", "0", Microsoft.Win32.RegistryValueKind.DWord)
+        My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System", "NoClose", "0", Microsoft.Win32.RegistryValueKind.DWord)
+        My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System", "NoLogoff", "0", Microsoft.Win32.RegistryValueKind.DWord)
+        My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System", "HideFastUserSwitching", "0", Microsoft.Win32.RegistryValueKind.DWord)
+        My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System", "EnableLUA", "1", Microsoft.Win32.RegistryValueKind.DWord)
+        My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System", "DisableRegistryTools", "0", Microsoft.Win32.RegistryValueKind.DWord)
+        My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System", "DisableTaskMgr", "0", Microsoft.Win32.RegistryValueKind.DWord)
+        My.Computer.Registry.SetValue("HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\System", "DisableCMD", "0", Microsoft.Win32.RegistryValueKind.DWord)
+        MsgBox("Votre PC a été débloqué.", MsgBoxStyle.Information, "Information")
+    End Sub
+End Class
